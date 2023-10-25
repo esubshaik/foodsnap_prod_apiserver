108c251a3d1665f52d31dacb98f09c03c7d032c5
Shaik Esub <108605272+EsubShaik@users.noreply.github.com>
2023-10-25 23:53:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 import csv
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for your app
 
 # Load the data from the CSV file into a dictionary
 data = {}
